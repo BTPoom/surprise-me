02006aca428bfea7a397f9caeacdb8de185f87a2
btpoom <vesbeg@gmail.com>
2026-08-03 17:23:32 +0700

    fix(upload): use private access for vercel blob store

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -11,7 +11,7 @@ export async function POST(request: Request) {
     }
 
     const blob = await put(filename, request.body, {
-      access: "public",
+      access: "private",
       addRandomSuffix: true,
     });
 
